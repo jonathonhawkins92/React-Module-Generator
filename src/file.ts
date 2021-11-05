cce084ca763c607a089731c49fad9ad77a5c1027
Goblin <jonathonhawkins92@gmail.com>
2021-11-05 16:12:18 +0000

    adding run time settings picker

--- src/file.ts
+++ src/file.ts
@@ -7,18 +7,24 @@ import templates from "./template";
 export interface NewFileSettings {
 	typescript: boolean;
 	fileExtensions: boolean;
+	includeStyle: boolean;
+	includeTranslation: boolean;
 	defaultImports: string[];
-	defaultModuleName: string;
 	exportType: "default" | "named" | "all";
 	rootDirectory: string;
+}
+
+enum SettingIds {
+	typescript = "typescript",
+	fileExtensions = "fileExtensions",
+	includeStyle = "includeStyle",
+	includeTranslation = "includeTranslation",
 }
 
 export default class FileController {
 	private settings: NewFileSettings;
 
 	private currentUri?: vscode.Uri;
-	private extension: string;
-	private reactExtension: string;
 
 	constructor(currentUri?: vscode.Uri) {
 		this.currentUri = currentUri ?? this.getUriOfCurrentFile();
@@ -35,64 +41,130 @@ export default class FileController {
 		this.settings = {
 			typescript: config.get("typescript", false),
 			fileExtensions: config.get("fileExtensions", false),
+			includeStyle: config.get("includeStyle", false),
+			includeTranslation: config.get("includeTranslation", false),
 			defaultImports: config.get("defaultImports", [
 				"import * as React from 'react';",
 			]),
-			defaultModuleName: config.get("defaultModuleName", "ModuleName"),
 			exportType: config.get("exportType", "all"),
 			rootDirectory: config.get("rootDirectory", ""),
 		};
-		if (this.settings.typescript) {
-			this.extension = `ts`;
-			this.reactExtension = `tsx`;
-		} else {
-			this.extension = `js`;
-			this.reactExtension = `jsx`;
-		}
 
 		return this;
 	}
 
-	private async getUserInput({
-		prompt,
-		placeholder,
-	}: {
-		prompt: string;
-		placeholder: string;
-	}) {
-		const inputBox = await vscode.window.createInputBox();
-		inputBox.prompt = prompt;
-		inputBox.placeholder = placeholder;
+	private async getUserSettings() {
+		console.log(this.settings);
+		console.log(this.settings.includeStyle);
+		console.log(this.settings.includeTranslation);
+		const settingsMap: Record<
+			string,
+			{
+				quickPick: vscode.QuickPickItem;
+				id: string;
+			}
+		> = {
+			"Use TypeScript": {
+				quickPick: {
+					alwaysShow: true,
+					label: "Use TypeScript",
+					description:
+						"Generate TypeScript files instead of Javascript files.",
+					picked: this.settings.typescript,
+				},
+				id: SettingIds.typescript,
+			},
+			"Include file extensions": {
+				quickPick: {
+					alwaysShow: true,
+					label: "Include file extensions",
+					description:
+						"Add the files extension to your barrel files export.",
+					picked: this.settings.fileExtensions,
+				},
+				id: SettingIds.fileExtensions,
+			},
+			"Include styles": {
+				quickPick: {
+					alwaysShow: true,
+					label: "Include styles",
+					description:
+						"Create a style file and import it in your component.",
+					picked: this.settings.includeStyle,
+				},
+				id: SettingIds.includeStyle,
+			},
+			"Include translations": {
+				quickPick: {
+					alwaysShow: true,
+					label: "Include translations",
+					description:
+						"Create a translation file and import it in your component.",
+					picked: this.settings.includeTranslation,
+				},
+				id: SettingIds.includeTranslation,
+			},
+		};
+		const settingsQuickPicks = Object.values(settingsMap).map(
+			(value) => value.quickPick
+		);
+
+		const input = await vscode.window.createQuickPick();
+		input.title = "Settings";
+		input.placeholder = "What's the name of your new module?";
+		input.matchOnDescription = false;
+		input.matchOnDetail = false;
+		input.items = settingsQuickPicks;
+		input.canSelectMany = true;
+		input.selectedItems = settingsQuickPicks.filter(
+			(value) => value.picked
+		);
+
 		let name = "";
+		let selectedIds: string[] = [];
 		await new Promise((resolve, reject) => {
 			let accepted = false;
-			inputBox.onDidChangeValue((value) => {
+			input.onDidChangeValue((value) => {
 				name = value;
 			});
-			inputBox.onDidAccept(() => {
+			input.onDidAccept(() => {
 				accepted = true;
-				inputBox.dispose();
+				input.dispose();
 				resolve(name);
 			});
-			inputBox.onDidHide(() => {
+			input.onDidChangeSelection((selection) => {
+				selectedIds = selection.map((item) => {
+					const id = item.label;
+					const setting = settingsMap[id];
+					return setting.id;
+				});
+			});
+			input.onDidHide(() => {
 				if (accepted) {
 					return;
 				}
 				reject("Input box hidden too early.");
 			});
-			inputBox.show();
+			input.show();
 		});
-		if (!name.trim()) {
-			throw new Error("invalid name");
+		if (!name || !name.trim()) {
+			throw new Error("Invalid module name");
 		}
-		return name;
-	}
-
-	private getModuleName() {
-		return this.getUserInput({
-			prompt: "What's the name of your new module?",
-			placeholder: this.settings.defaultModuleName,
-		});
+		const settings = {
+			[SettingIds.typescript]: selectedIds.includes(
+				SettingIds.typescript
+			),
+			[SettingIds.fileExtensions]: selectedIds.includes(
+				SettingIds.fileExtensions
+			),
+			[SettingIds.includeStyle]: selectedIds.includes(
+				SettingIds.includeStyle
+			),
+			[SettingIds.includeTranslation]: selectedIds.includes(
+				SettingIds.includeTranslation
+			),
+		};
+		return { name, settings };
 	}
 
 	private async getRoot(value?: string) {
@@ -116,21 +188,38 @@ export default class FileController {
 		return this.settings.rootDirectory;
 	}
 
+	private getExtension(typescript: boolean) {
+		if (typescript) {
+			return {
+				extension: "ts",
+				reactExtension: "tsx",
+			};
+		} else {
+			return {
+				extension: "js",
+				reactExtension: "jsx",
+			};
+		}
+	}
+
 	public async add(targetDir?: string) {
 		if (!targetDir) {
 			targetDir = await this.getDir();
 		}
 		const root = await this.getRoot(targetDir);
-		const name = await this.getModuleName();
+		const { name, settings } = await this.getUserSettings();
+		const { extension, reactExtension } = this.getExtension(
+			settings.typescript
+		);
 
 		const componentName = this.normalizeComponentName(name);
-		const componentFile = `${componentName}.${this.reactExtension}`;
-		const componentFilename = this.settings.fileExtensions
+		const componentFile = `${componentName}.${reactExtension}`;
+		const componentFilename = settings.fileExtensions
 			? componentFile
 			: componentName;
 		const componentPath = path.join(root, componentFile);
 
-		const barrelFile = `index.${this.extension}`;
+		const barrelFile = `index.${extension}`;
 		const barrelPath = path.join(root, barrelFile);
 		const isBarrel = await fs.stat(barrelPath);
 		if (!isBarrel.isFile()) {
@@ -160,7 +249,8 @@ export default class FileController {
 		);
 		await fs.writeFile(componentPath, componentTemplate);
 
-		vscode.window.showInformationMessage(componentPath);
+		const doc = await vscode.workspace.openTextDocument(componentPath);
+		vscode.window.showTextDocument(doc);
 	}
 
 	public async create(targetDir?: string) {
@@ -168,14 +258,17 @@ export default class FileController {
 			targetDir = await this.getDir();
 		}
 		const root = await this.getRoot(targetDir);
-		const name = await this.getModuleName();
+		const { name, settings } = await this.getUserSettings();
+		const { extension, reactExtension } = this.getExtension(
+			settings.typescript
+		);
 
 		const dirName = this.normalizeDirName(name);
 		const dirPath = path.join(root, dirName);
 
 		const componentName = this.normalizeComponentName(name);
-		const componentFile = `${componentName}.${this.reactExtension}`;
-		const componentFilename = this.settings.fileExtensions
+		const componentFile = `${componentName}.${reactExtension}`;
+		const componentFilename = settings.fileExtensions
 			? componentFile
 			: componentName;
 		const componentPath = path.join(dirPath, componentFile);
@@ -187,7 +280,7 @@ export default class FileController {
 			}
 		);
 
-		const barrelFile = `index.${this.extension}`;
+		const barrelFile = `index.${extension}`;
 		const barrelPath = path.join(dirPath, barrelFile);
 		const barrelTemplate = await this.generateBarrelTemplate(
 			componentName,
@@ -201,7 +294,8 @@ export default class FileController {
 		await fs.writeFile(barrelPath, barrelTemplate);
 		await fs.writeFile(componentPath, componentTemplate);
 
-		vscode.window.showInformationMessage(componentPath);
+		const doc = await vscode.workspace.openTextDocument(componentPath);
+		vscode.window.showTextDocument(doc);
 	}
 
 	public async explorerCreate(filePath: string) {
